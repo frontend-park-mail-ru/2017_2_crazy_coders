9c6112f953b3c22cf70abe3cb0fa05375d20f7ea
Max Samokhin <samokhinmax@gmail.com>
2017-09-16 10:39:26 +0300

    add module valid form

---
+++ public/form/valid-form/validRegistrForm.js
@@ -0,0 +1,83 @@
+(function () {
+
+    "use strict";
+
+    function hideError(form) {
+        let removeErrorCollection = form.getElementsByClassName('error-msg');
+        const removeErrorArray = Array.from(removeErrorCollection);
+        removeErrorArray.forEach(elem => {
+            elem.remove();
+        });
+    }
+
+    function isCorrectLogin(login) {
+        return login.trim().length > 4;
+    }
+
+    function isCorrectPassword(pswd) {
+        return pswd.trim().length >= 6;
+    }
+
+    function isCorrectEmail(email) {
+        return email.match(/^[0-9a-z-\.]+\@[0-9a-z-]{2,}\.[a-z]{2,}$/i);
+    }
+
+    function isSamePasswords(pswd, pswdRepeat, form) {
+        return pswd === pswdRepeat;
+    }
+
+    function createErrorElement(msg) {
+        let errorElement = document.createElement('p');
+        errorElement.textContent = msg;
+        errorElement.classList.add('error-msg');
+
+        return errorElement;
+    }
+
+    class ValidRegisterForm {
+        constructor(login, email, password, repeatPassword, form) {
+            this.login = login;
+            this.email = email;
+            this.password = password;
+            this.repeatPassword = repeatPassword;
+            this.currentForm = form;
+        }
+
+        validRegisterForm() {
+
+            hideError(this.currentForm);
+
+            let flag = true;
+            const loginField = this.currentForm.children[0],
+                emailField = this.currentForm.children[1],
+                passwordField = this.currentForm.children[2],
+                repeatPasswordField = this.currentForm.children[3];
+
+            if (!isCorrectLogin(this.login)) {
+                flag = false;
+                this.currentForm.insertBefore(createErrorElement('invalid login (<=4 symbol)'), loginField);
+            }
+
+            if (!isCorrectEmail(this.email)) {
+                flag = false;
+                this.currentForm.insertBefore(createErrorElement('invalid email'), emailField);
+            }
+
+            if (!isCorrectPassword(this.password)) {
+                flag = false;
+                this.currentForm.insertBefore(createErrorElement('invalid password (<6 symbol)'), passwordField);
+            }
+
+            if (!isSamePasswords(this.password, this.repeatPassword)) {
+                flag = false;
+                this.currentForm.insertBefore(createErrorElement('the values of entered passwords do not match'), repeatPasswordField);
+            }
+
+            return flag;
+        }
+
+    }
+
+    window.ValidRegisterForm = ValidRegisterForm;
+
+})();
---
+++ public/main.js
@@ -0,0 +1,258 @@
+(function () {
+
+    "use strict";
+
+    const ValidLoginFrom = window.ValidLoginForm;
+    const ValidRegisterForm = window.ValidRegisterForm;
+
+    function isAuthUser(callback) {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '/isauth', true);
+        xhr.withCredentials = true;
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== 4) return;
+            if (+xhr.status !== 200) return;
+            const response = JSON.parse(xhr.responseText);
+            callback(null, response);
+        };
+
+        xhr.send();
+    }
+
+    function exit() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('POST', '/exit', true);
+        xhr.withCredentials = true;
+        xhr.send();
+    }
+
+    function register(login, email, password, callback) {
+        const user = {login, email, password};
+        const body = JSON.stringify(user);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/register', true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        xhr.withCredentials = true;
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (+xhr.status !== 200) {
+                return callback(xhr);
+            }
+            const response = JSON.parse(xhr.responseText);
+            callback(null, response);
+        };
+        xhr.send(body);
+    }
+
+    function login(login, password, callback) {
+        const user = {login, password};
+        const body = JSON.stringify(user);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', '/login', true);
+        xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
+        xhr.withCredentials = true;
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+            if (+xhr.status !== 200) {
+                return callback(xhr);
+            }
+            const response = JSON.parse(xhr.responseText);
+            callback(null, response);
+        };
+        xhr.send(body);
+    }
+
+//элементы страницы, для навигации
+    const allSectionCollection = document.getElementsByTagName('section');
+    const allSectionArray = Array.from(allSectionCollection);
+
+    const [helloPage, loginPage, registryPage, mainPage, musicSection,
+        scorePage, aboutPage, buttomsPanel] = allSectionArray;
+
+    function hideAll(arr) {
+        arr.forEach(elem => {
+            if (elem.id !== 'buttons-panel') {
+                elem.hidden = true;
+            }
+        })
+    }
+
+    function isUnregisteredUser() { // если пользователь зарегестрирован
+        buttomsPanel.hidden = false; // всегда можно посмотреть счет, о нас, выключить звук
+        helloPage.hidden = false;
+        mainPage.hidden = true;
+
+        allSectionArray.forEach(section => { // удаляем события, если они есть
+            section.removeEventListener("click", {}, false);
+        });
+
+        allSectionArray.forEach(section => {
+            section.addEventListener('click', (event) => {
+                const elemId = event.target.getAttribute('id');
+                switch (elemId) {
+                    case 'button-log':
+                        helloPage.hidden = true;
+                        loginPage.hidden = false;
+                        break;
+
+                    case 'button-register':
+                        helloPage.hidden = true;
+                        registryPage.hidden = false;
+                        break;
+
+                    case 'back-login':
+                        loginPage.hidden = true;
+                        registryPage.hidden = true;
+                        helloPage.hidden = false;
+                        break;
+
+                    case 'back-signup':
+                        loginPage.hidden = true;
+                        registryPage.hidden = true;
+                        helloPage.hidden = false;
+                        break;
+
+                    case 'score-logo':
+                        hideAll(allSectionArray);
+                        scorePage.hidden = false;
+                        break;
+
+                    case 'about-logo':
+                        hideAll(allSectionArray);
+                        aboutPage.hidden = false;
+                        break;
+
+                    case 'back-about':
+                        section.hidden = true;
+                        helloPage.hidden = false;
+                        mainPage.hidden = true;
+                        break;
+
+                    case 'back-score':
+                        section.hidden = true;
+                        helloPage.hidden = false;
+                        mainPage.hidden = true;
+                        break;
+                }
+            }, false);
+        });
+    }
+
+    function isRegisteredUser() { // если пользователь зарегестрирован
+        buttomsPanel.hidden = false;
+        helloPage.hidden = true;
+        mainPage.hidden = false;
+
+        allSectionArray.forEach(section => { // удаляем события, если они есть
+            section.removeEventListener("click", {}, false);
+        });
+
+        allSectionArray.forEach(section => {
+            section.addEventListener('click', (event) => {
+                const elemId = event.target.getAttribute('id');
+                switch (elemId) {
+                    case 'logout':
+                        console.log('i am logout');
+                        exit();
+                        isUnregisteredUser();
+                        break;
+                    case 'start-game':
+                        console.log('i am start');
+                        break;
+                    case 'score-logo':
+                        hideAll(allSectionArray);
+                        scorePage.hidden = false;
+                        break;
+                    case 'about-logo':
+                        hideAll(allSectionArray);
+                        aboutPage.hidden = false;
+                        break;
+                    case 'back-score':
+                        section.hidden = true;
+                        mainPage.hidden = false;
+                        helloPage.hidden = true;
+                        break;
+                    case 'back-about':
+                        section.hidden = true;
+                        mainPage.hidden = false;
+                        helloPage.hidden = true;
+                        break;
+                }
+            })
+        });
+    }
+
+    window.onload = () => {
+        isAuthUser((error, resp) => {
+            if (resp.success === 'yes') {
+                helloPage.hidden = true;
+                isRegisteredUser();
+                console.log('lasudv');
+            } else {
+                mainPage.hidden = true;
+                isUnregisteredUser();
+                console.log('aasdalksdn');
+            }
+        });
+
+        let registryForm = document.getElementById('registry-form');
+        let loginForm = document.getElementById('login-form');
+
+        registryForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            let userLogin = registryForm.elements['login'].value;
+            let userEmail = registryForm.elements['email'].value;
+            let userPassword = registryForm.elements['password'].value;
+            let userRepeatPassword = registryForm.elements['repeat-password'].value;
+
+            const isValid = new ValidRegisterForm(userLogin, userEmail, userPassword, userRepeatPassword, registryForm);
+
+            if (isValid.validRegisterForm()) {
+                register(userLogin, userEmail, userPassword, (err, resp) => {
+                    if (err) {
+                        return alert(`AUTH Error: ${err.status}`);
+                    }
+                    if (resp.response === 200) {
+                        registryPage.hidden = true;
+                        helloPage.hidden = true;
+                        isRegisteredUser();
+                    }
+                    registryForm.reset();
+                });
+            }
+        });
+
+        loginForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+            let userLogin = loginForm.elements['login'].value;
+            let userPassword = loginForm.elements['password'].value;
+
+
+            const isValid = new ValidLoginForm(userLogin, userPassword, loginForm);
+
+            if (isValid.validLoginForm()) {
+                login(userLogin, userPassword, (err, resp) => {
+                    if (err) {
+                        return alert(`AUTH Error: ${err.status}`);
+                    }
+
+                    if (resp.success === 'yes') {
+                        loginPage.hidden = true;
+                        helloPage.hidden = true;
+                        isRegisteredUser();
+                    }
+                    loginForm.reset();
+                });
+            }
+        });
+    }
+
+})();
