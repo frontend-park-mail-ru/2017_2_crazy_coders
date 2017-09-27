6a3abeeee2eb872788d4ab8753a87c0618de408e
Max Samokhin <samokhinmax@gmail.com>
2017-09-28 00:50:16 +0300

    add jsDoc-block

--- public/components/Block/BlockComponents.js
+++ public/components/Block/BlockComponents.js
@@ -1,7 +1,17 @@
 'use strict';
 
+/**
+ * Базовый класс блока
+ * @module Block
+ */
 export default class Block {
-
+    /**
+     * @param {string} [tagName='div'] - tagName блока
+     * @param {*} [attrs={}] - объект с атрибутами блока
+     * @param {string[]} [classes=[]] - список имён классов
+     * @param {*} [data={}] - объект с данными блока
+     * @constructor
+     */
     constructor(tagName = 'div', attrs = {}, classes = [], data = {}) {
         this.el = document.createElement(tagName);
 
@@ -16,45 +26,81 @@ export default class Block {
         this.setData(data);
     }
 
-
+    /**
+     * Установить новые данные блока
+     * @param {string} data
+     */
     setData(data) {
         this.data = data;
     }
 
+    /**
+     * Получить данные блока
+     */
     getData() {
         return this.data;
     }
 
+    /**
+     * Установить HTML содержимое блока
+     * @param {HTMLElement} html
+     */
     setHTML(html) {
         this.el.innerHTML = html;
     }
 
+    /**
+     * Устанавливает новый текст для элемента
+     * @param {string} text - устанавливаем текст
+     */
     setText(text) {
         this.el.textContent = text;
     }
 
-
+    /**
+     * Очищаем содержимое элемента
+     */
     clear() {
         this.el.innerHTML = '';
     }
 
+    /**
+     * Скрывает элемент
+     */
     hide() {
         this.el.setAttribute('hidden', 'hidden');
     }
 
+    /**
+     * Отображает элемент
+     */
     show() {
         this.el.removeAttribute('hidden');
     }
 
+    /**
+     * Возвращает элемент
+     * @returns {HTMLElement}
+     */
     getElement() {
         return this.el;
     }
 
+    /**
+     * Вставляет в себя элемент
+     * @param {HTMLElement} block
+     */
     append(block) {
         this.getElement().appendChild(block);
         return this;
     }
 
+    /**
+     * Позволяет подписаться на событие
+     * @param {string} event
+     * @param {EventListener} callback
+     * @return {function(this:Block)} - функция отписки от события
+     */
     on(event, callback) {
         this.el.addEventListener(event, callback);
         return function () {
