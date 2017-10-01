a3e9f575439d74d9bba7faf30b70e901ae09ec5c
grigo <grigorevpv@gmail.com>
2017-10-01 22:38:45 +0300

    add ScoreListController

---
+++ public/controllers/ScoreListController.js
@@ -0,0 +1,42 @@
+'use strict';
+
+import Controller from "./Controller";
+
+class ScoreListController extends Controller {
+
+    constructor(opt = {}) {
+        if(ScoreListController.__instance) {
+            return ScoreListController.__instance;
+        }
+
+        super(opt);
+        ScoreListController.__instance = this;
+        this.addListener();
+    }
+
+    addListener() {
+
+        document.getElementById('score-button-back').addEventListener('click', event => {
+            event.preventDefault();
+            this.router.go('/');
+        });
+
+
+    }
+
+    resume() {
+        this.show();
+    }
+
+    show() {
+        this.page_parts.get("Header").show();
+        this.page_parts.get("Scoreboard").show();
+    }
+
+    hide() {
+        this.page_parts.get("Header").hide();
+        this.page_parts.get("Scoreboard").hide();
+    }
+}
+
+export default ScoreListController;
