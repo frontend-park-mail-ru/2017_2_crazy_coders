b613fc475ec2d7960fbade1b9c7133f3c87dfb81
Max Samokhin <samokhinmax@gmail.com>
2017-10-27 11:26:05 +0300

    add scope player

--- public/game/classes/World/World.js
+++ public/game/classes/World/World.js
@@ -103,7 +103,7 @@ class World extends Phaser.State {
 		this.pauseButton.clicked = false;
 
 
-		this.gameTime = 10;
+		this.gameTime = 20;
 		if (!this.timer) {
 			this.timePause = 0;
 			this.isPaused = false;
@@ -157,11 +157,6 @@ class World extends Phaser.State {
 	}
 
 	update() {
-		if (this.gameTime - this.total < 0) {
-			this.total = 0;
-			this.game.state.start('GameOverMenu', true, false);
-		}
-
 		//убираем перекрытие объектов
 		this.physics.arcade.overlap(this.enemyBullets, this.tank, this.bulletHitPlayer, null, this);
 		// враги живые
@@ -177,7 +172,6 @@ class World extends Phaser.State {
 				this.enemies[i].update();
 			}
 		}
-
 
 		// величина угла поворота
 		if (this.cursors.left.isDown) {
@@ -219,8 +213,20 @@ class World extends Phaser.State {
 			this.fire();
 		}
 
-		let text = new Text(this, 5, 5, 'hello');
-		// this.debug.text('Hp: ' + this.health + '     Count opponents:' + this.enemiesAlive, 42, 42);
+		if (this.gameTime - this.total < 0 || this.enemiesAlive === 0 || this.health <= 0) {
+
+
+			this.score = (this.gameTime - this.total) * 50 + (this.enemiesTotal - this.enemiesAlive) * 50;
+
+			if(this.health <= 0) {
+				this.score = 0;
+			}
+
+
+			console.log(`total: ${this.score}`);
+
+			this.game.state.start('GameOverMenu', true, false);
+		}
 	}
 
 	bulletHitEnemy(tank, bullet) {
@@ -257,7 +263,6 @@ class World extends Phaser.State {
 		this.total++;
 		this.game.debug.text(this.formatTime(Math.round(this.gameTime - this.total)), this.world.centerX, 14, "#ff0" );
 		this.game.debug.text('Hp: ' + this.health + '     Count opponents:' + this.enemiesAlive, 84, 42,"#ff0");
-		// console.log(`total: ${this.total}`);
 	}
 
 	formatTime(s) {
