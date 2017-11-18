26620bb159a4659d6034b314358938976b76e11d
grigorevpv <lomogi99>
2017-11-18 18:13:30 +0300

    add classes TankBody and TankTurret

--- public/game/classes/Tank/Tank.ts
+++ public/game/classes/Tank/Tank.ts
@@ -1,13 +1,16 @@
+import TankBody from './TankBody/TankBody';
+import Turret from './TankTurret/TankTurret';
+import Ex from './Ex';
+
 export default class TankState extends Phaser.Sprite {
     _game: Phaser.Game;
-    _tank: Phaser.Sprite;
-    _turret: Phaser.Sprite;
+    _tank: TankBody;
+    _turret: Turret;
     _health: number;
     _fireRate: number;
     _nextFire: number;
     _xPosition: number;
     _yPosition: number;
-    _currentSpeed: number;
     _alive: boolean;
     _cursor: Phaser.CursorKeys;
 
@@ -25,46 +28,16 @@ export default class TankState extends Phaser.Sprite {
 
     create(): void {
 
-        this._tank = this._game.add.sprite(50, 400, 'tank', 'tank1');
-        this._tank.anchor.setTo(0.5, 0.5);
-        this._game.physics.enable(this._tank, Phaser.Physics.ARCADE);
-
-        this._tank.body.maxVelocity.setTo(100, 100);
-        this._tank.body.collideWorldBounds = true;
-        this._turret = this._game.add.sprite(50, 400, 'tank', 'turret');
-        this._turret.anchor.setTo(0.5, 0.5);
-
         this._cursor = this._game.input.keyboard.createCursorKeys();
+        this._tank = new TankBody(this._game, this._cursor);
+        this._turret = new Turret(this._game, this._cursor);
+
 
     }
 
     update(): void {
-        // величина угла поворота
-        if (this._cursor.left.isDown) {
-            this._tank.angle -= 5;
-        }
-        else if (this._cursor.right.isDown) {
-            this._tank.angle += 5;
-        }
-
-        // скорость
-        if (this._cursor.up.isDown) {
-            this._currentSpeed = 210;
-        } else {
-            if (this._currentSpeed > 0) {
-                this._currentSpeed -= 100; // скорость торможения
-            }
-        }
-
-        // движение и поворотами
-        if (this._currentSpeed > 0) {
-            this._game.physics.arcade.velocityFromRotation(this._tank.rotation, this._currentSpeed, this._tank.body.velocity);
-        }
-
-        // привязываем башню к танку
-        this._turret.x = this._tank.x;
-        this._turret.y = this._tank.y;
-
-        this._turret.rotation = this._game.physics.arcade.angleToPointer(this._turret);
+        this._tank.update();
+        this._turret.turretCoordinate = this._tank.currentPosition;
+        this._turret.update();
     }
 }
---
+++ public/game/classes/Tank/TankTurret/TankTurret.ts
@@ -0,0 +1,27 @@
+export default class TankTurret extends Phaser.Sprite {
+    _game: Phaser.Game;
+    _turret: Phaser.Sprite;
+    _cursor: Phaser.CursorKeys;
+
+
+    constructor(game: Phaser.Game, cursor: Phaser.CursorKeys) {
+        super(game, 0, 0);
+        this._game = game;
+        this._cursor = cursor;
+        this.create();
+    }
+
+    create(): void {
+        this._turret = this._game.add.sprite(50, 400, 'tank', 'turret');
+        this._turret.anchor.setTo(0.5, 0.5);
+    }
+
+    update(): void {
+        this._turret.rotation = this._game.physics.arcade.angleToPointer(this._turret);
+    }
+
+    public set turretCoordinate(coordinate) {
+        this._turret.x = coordinate.xCoordinate;
+        this._turret.y = coordinate.yCoordinate;
+    }
+}
