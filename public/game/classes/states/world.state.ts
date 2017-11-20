5d4d2d306572e10f096a3819aef6071cd1b1d701
grigo <grigorevpv@gmail.com>
2017-11-21 00:25:33 +0300

    merge game-dev and developer branches

--- public/game/classes/states/world.state.ts
+++ public/game/classes/states/world.state.ts
@@ -64,6 +64,7 @@ export default class WorldState extends State {
 
     };
 
+
     randomInteger(min: number, max: number): number {
         let rand = min - 0.5 + Math.random() * (max - min + 1);
         rand = Math.round(rand);
