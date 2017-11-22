e9c45da6b0de3230c8a0f9d0a1311bf60296eb37
Max Samokhin <samokhinmax@gmail.com>
2017-11-23 00:07:03 +0300

    add test

---
+++ test/test.js
@@ -0,0 +1,32 @@
+import chai from 'chai';
+import UserService from '../public/services/UserService';
+
+mocha.setup('bdd');
+let expect = chai.expect;
+
+let userEmail = 'qweq@qweq.qweq';
+let userPassword = 'qweqqweq';
+
+describe('User Service', function () {
+	describe('Log In', function () {
+		it('success', function (done) {
+			new UserService()
+				.signIn(userEmail, userPassword)
+				.then(response => {
+					expect(response).to.be.not.empty;
+					done();
+				});
+		});
+		it('error', function (done) {
+			userEmail = 'error@error.error';
+			new UserService()
+				.signIn(userEmail, userPassword)
+				.catch(err => {
+					done();
+				});
+		});
+	});
+});
+
+mocha.checkLeaks();
+mocha.run();
